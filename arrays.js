c8b5b34fcf483e8ce6bf2f79e7f3801ea224e569
Stephen Robert hall <steve.r.hall@outlook.com>
2019-08-18 15:55:27 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,22 +1,22 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(bars, element) {
-  var newarray = element, ...bars;
+  var newarray = [element, ...bars];
   return newarray
 }
 
 function destructivelyAddElementToBeginningOfArray(bars, element) {
-  bars = element, ...bars;
+  bars = [element, ...bars];
   return bars
 }
 
 
 function addElementToEndOfArray(bars, element) {
-  var newbars = ...bars, element;
+  var newbars = [...bars, element];
   return newbars
 }
 
 function destructivelyAddElementToEndOfArray(bars, element) {
-  bars = ...bars, element;
+  bars = [...bars, element];
   return bars
 }
