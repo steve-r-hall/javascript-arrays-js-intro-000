ce60ff8a083cc0422c23595d3a4bf316e7ffae2b
Stephen Robert hall <steve.r.hall@outlook.com>
2019-08-18 16:20:46 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -21,7 +21,7 @@ function destructivelyAddElementToEndOfArray(bars, element) {
   return bars
 }
 
-function accessElementInArray(bars,occurrence) {
+function accessElementInArray(bars, occurrence) {
   var reqentry = bars(occurrence);
   return reqentry
 }
@@ -33,5 +33,15 @@ function destructivelyRemoveElementFromBeginningOfArray(bars) {
 
 function removeElementFromBeginningOfArray(bars) {
   var newbars = bars.slice(1)
+  return newbars
+}
+
+function destructivelyRemoveElementFromEndOfArray(bars) {
+bars.pop();
+return bars
+}
+
+function removeElementFromEndOfArray(bars) {
+  var newbars = bars.slice(0, bars.length -1)
   return newbars
 }
