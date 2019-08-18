b62a0b759a76ba64ddcf1cd87799e2be36e8a59f
Stephen Robert hall <steve.r.hall@outlook.com>
2019-08-18 15:42:16 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,22 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+  var newarray = element, ...array;
+  return newarray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array = element, ...array;
+  return array
+}
+
+
+function addElementToEndOfArray(array, element) {
+  var newarray = ...array, element;
+  return newarray
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array = ...array, element;
+  return array
+}
